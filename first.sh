4ae56c01db308eeefb3d003d4fd92a516f6b8ff5
mencim <46687537+mencim@users.noreply.github.com>
2024-07-11 15:44:44 +0300

    Update first.sh

--- first.sh
+++ first.sh
@@ -5,9 +5,9 @@ PermitRootLogin yes
 }' /etc/ssh/sshd_config
 
 echo  >> /etc/network/interfaces
-echo "#iface !!!!!! inet static" >> /etc/network/interfaces
-echo "#        address 192.168.!.!/!" >> /etc/network/interfaces
-echo "#        gateway 192.168.!.!" >> /etc/network/interfaces
+echo "#iface ! inet static" >> /etc/network/interfaces
+echo "#        address 192.168../24" >> /etc/network/interfaces
+echo "#        gateway 192.168.." >> /etc/network/interfaces
 echo "#        dns-nameservers 8.8.8.8" >> /etc/network/interfaces
 echo  >> /etc/network/interfaces
 nano /etc/network/interfaces
