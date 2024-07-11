fe603db4be491e649a518d44d9dd608e139b6b26
mencim <46687537+mencim@users.noreply.github.com>
2024-07-11 15:19:54 +0300

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sed -i '/^PermitRootLogin/{s/^/#/;a\
+PermitRootLogin yes
+}' /etc/ssh/sshd_config
+
+echo  >> /etc/network/interfaces
+echo "#iface !!!!!! inet static" >> /etc/network/interfaces
+echo "#        address 192.168.!.!/!" >> /etc/network/interfaces
+echo "#        gateway 192.168.!.!" >> /etc/network/interfaces
+echo "#        dns-nameservers 8.8.8.8" >> /etc/network/interfaces
+echo  >> /etc/network/interfaces
+nano /etc/network/interfaces
